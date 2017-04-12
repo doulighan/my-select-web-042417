cc59a0177758abfbe1716dda6f571b01b63e101e
doulighan <doulighan@gmail.com>
2017-04-12 12:54:07 -0400

    adjusted indentation

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,11 @@
 def my_select(collection)
-	i = 0
-	modified_collection = []
-	while i < collection.length
-			if yield(collection[i])
-				modified_collection << collection[i]
-			end
-			i += 1
-	end
-	modified_collection
+  i = 0
+  modified_collection = []
+  while i < collection.length
+      if yield(collection[i])
+        modified_collection << collection[i]
+      end
+      i += 1
+  end
+  modified_collection
 end
